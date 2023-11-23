ed45843572ce37c6fa7734b3879696f469aa1ad8
DymitrDworakowski <116100679+DymitrDworakowski@users.noreply.github.com>
2023-11-23 15:40:13 +0100

    fix modal

--- n4/src/components/Modal.jsx
+++ n4/src/components/Modal.jsx
@@ -1,30 +1,38 @@
-import{ Component } from "react";
+import { Component } from "react";
 import * as basicLightbox from "basiclightbox";
-import './Modal.css';
+import  css  from './Modal.module.css';
 
 class Modal extends Component {
   componentDidMount() {
     this.openModal();
   }
-   
-  handleKeyDown = event => {
-    if (event.code === 'Escape') {
+
+  handleKeyDown = (event) => {
+    if (event.code === "Escape") {
       this.props.onClose(); // Закрываем модальное окно при нажатии клавиши Escape
     }
   };
+  handleBackdropClick = (event) => {
+    if (event.currentTarget === event.target) {
+      this.props.onClose(); // Закрываем модальное окно при клике на фон
+    }
+  };
+openModal() {
+  const { largeImageURL } = this.props;
 
-  openModal() {
-    const { largeImageURL, onClose } = this.props;
-
-    const instance = basicLightbox.create(`
-  <div className="overlay" onClick="${onClose}">
-    <div className="modal">
-      <img src="${largeImageURL}" width="800" height="600">
+  const instance = basicLightbox.create(`
+    <div class="${css.overlay}">
+      <div class="${css.modal}">
+        <img src="${largeImageURL}" width="800" height="600">
+      </div>
     </div>
-  </div>
-`);
-    instance.show();
-  }
+  `);
+
+  const overlayElement = instance.element().querySelector(`.${css.overlay}`);
+  overlayElement.addEventListener("click", this.handleBackdropClick);
+
+  instance.show();
+}
 
   render() {
     return null;
