909994c96eda3d7e87121ffb699767cec31b73a5
DymitrDworakowski <116100679+DymitrDworakowski@users.noreply.github.com>
2023-11-24 11:55:51 +0100

    fix modal v2

--- n4/src/components/ImageGallery.jsx
+++ n4/src/components/ImageGallery.jsx
@@ -1,42 +1,23 @@
+import React, { Component } from "react";
 
-import React, { Component } from 'react';
-import Modal from './Modal';
-import './ImageGallery.css';
-import ImageGalleryItem from './ImageGalleryItem';
+import "./ImageGallery.css";
+import ImageGalleryItem from "./ImageGalleryItem";
 
 class ImageGallery extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      selectedImageId: null,
-    };
-  }
-
-  openModal = (id) => {
-    this.setState({ selectedImageId: id });
-  };
-
-  closeModal = () => {
-    this.setState({ selectedImageId: null });
-  };
-
   render() {
     const { images } = this.props;
-    const { selectedImageId } = this.state;
 
     return (
-      
-      <div className='imgGal'>
-        <ImageGalleryItem images={images} openModal={this.openModal} />
-          {selectedImageId && (
-          <Modal
-            largeImageURL={images.find((image) => image.id === selectedImageId)?.largeImageURL}
-            onClose={this.closeModal}
+      <div className="imgGal">
+        {images.map(({ id, webformatURL, tags, largeImageURL }) => (
+          <ImageGalleryItem
+            key={id}
+            webformatURL={webformatURL}
+            tags={tags}
+            largeImageURL={largeImageURL}
           />
-        )}
-        
-      </div> 
-     
+        ))}
+      </div>
     );
   }
 }
--- n4/src/components/Modal.jsx
+++ n4/src/components/Modal.jsx
@@ -1,41 +1,34 @@
 import { Component } from "react";
-import * as basicLightbox from "basiclightbox";
-import  css  from './Modal.module.css';
+import './Modal.css';
+import Rodal from 'rodal';
+import 'rodal/lib/rodal.css';
 
 class Modal extends Component {
-  componentDidMount() {
-    this.openModal();
-  }
+
+
+
 
   handleKeyDown = (event) => {
-    if (event.code === "Escape") {
-      this.props.onClose(); // Закрываем модальное окно при нажатии клавиши Escape
+    if (event.code === "Escape") { 
+      this.props.onClose();
     }
   };
-  handleBackdropClick = (event) => {
-    if (event.currentTarget === event.target) {
-      this.props.onClose(); // Закрываем модальное окно при клике на фон
-    }
-  };
-openModal() {
-  const { largeImageURL } = this.props;
 
-  const instance = basicLightbox.create(`
-    <div class="${css.overlay}">
-      <div class="${css.modal}">
-        <img src="${largeImageURL}" width="800" height="600">
-      </div>
-    </div>
-  `);
-
-  const overlayElement = instance.element().querySelector(`.${css.overlay}`);
-  overlayElement.addEventListener("click", this.handleBackdropClick);
-
-  instance.show();
-}
 
   render() {
-    return null;
+   
+const { largeImageURL,tags,openModal,onClose } = this.props;
+    return (
+      <div className="overlay" >
+        
+        <Rodal  visible={openModal} closeOnEsc={true} onClose={onClose}>
+        <div className="modal">
+          <img src={largeImageURL} alt={tags} />
+        </div>
+        </Rodal>
+        
+      </div>
+    );
   }
 }
 
