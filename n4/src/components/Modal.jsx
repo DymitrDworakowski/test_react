5dfe9b39ddf2b8b12763209a00013c899849d5b8
DymitrDworakowski <116100679+DymitrDworakowski@users.noreply.github.com>
2023-11-24 12:29:49 +0100

    Update Modal.jsx

--- n4/src/components/Modal.jsx
+++ n4/src/components/Modal.jsx
@@ -21,10 +21,14 @@ const { largeImageURL,tags,openModal,onClose } = this.props;
     return (
       <div className="overlay" >
         
-        <Rodal  visible={openModal} closeOnEsc={true} onClose={onClose}>
-        <div className="modal">
-          <img src={largeImageURL} alt={tags} />
-        </div>
+        <Rodal customStyles={{background:'none'}} width={800} height={600} visible={openModal} closeOnEsc={true} onClose={onClose}>
+          <div className="modal">
+            <img
+              src={largeImageURL}
+              alt={tags}
+              style={{ maxWidth: "100%", maxHeight: "100%", display: "block", margin: "auto" }}
+            />
+          </div>
         </Rodal>
         
       </div>
