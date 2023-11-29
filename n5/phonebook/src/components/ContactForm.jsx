fc7821bb79070a646b98cefbb74568d0ef6254c8
DymitrDworakowski <116100679+DymitrDworakowski@users.noreply.github.com>
2023-11-29 15:32:41 +0100

    Update ContactForm.jsx

--- n5/phonebook/src/components/ContactForm.jsx
+++ n5/phonebook/src/components/ContactForm.jsx
@@ -1,19 +1,15 @@
-
 import { nanoid } from "nanoid"; // Імпорт nanoid
 import { useState } from "react";
 
 const ContactForm = ({ contacts, onAddContact }) => {
-  
-   const [formData, setFormData] = useState({
+  const [formData, setFormData] = useState({
     name: "",
     number: "",
-   });
-  
-  
+  });
+
   const handleSubmit = (evt) => {
     evt.preventDefault();
 
-   
     // Отримайте функцію onAddContact через props
 
     if (contacts.some((contact) => contact.name === formData.name)) {
@@ -34,44 +30,43 @@ const ContactForm = ({ contacts, onAddContact }) => {
       number: "",
     });
   };
-  
-const handleChangeInput = (evt) => {
-  // Деструктуризація подій для отримання name та value зі змінної target
-  const { name, value } = evt.target;
 
-  // Використовуємо setFormData для оновлення стану
-  setFormData((prevData) => ({
-    // Використовуємо розпросторення для копіювання попереднього стану
-    ...prevData,
-    // Оновлюємо значення відповідного поля (name) з новим значенням (value)
-    [name]: value,
-  }));
+  const handleChangeInput = (evt) => {
+    // Деструктуризація подій для отримання name та value зі змінної target
+    const { name, value } = evt.target;
+
+    // Використовуємо setFormData для оновлення стану
+    setFormData((prevData) => ({
+      // Використовуємо розпросторення для копіювання попереднього стану
+      ...prevData,
+      // Оновлюємо значення відповідного поля (name) з новим значенням (value)
+      [name]: value,
+    }));
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <p>Name</p>
+      <input
+        type="text"
+        name="name"
+        required
+        placeholder="Name"
+        value={formData.name}
+        onChange={handleChangeInput}
+      />
+      <p>Number</p>
+      <input
+        type="tel"
+        name="number"
+        required
+        placeholder="Number"
+        value={formData.number}
+        onChange={handleChangeInput}
+      />
+      <button type="submit">Add contact</button>
+    </form>
+  );
 };
-
-    return (
-      <form onSubmit={handleSubmit}>
-        <p>Name</p>
-        <input
-          type="text"
-          name="name"
-          required
-          placeholder="Name"
-          value={formData.name}
-          onChange={handleChangeInput}
-        />
-        <p>Number</p>
-        <input
-          type="tel"
-          name="number"
-          required
-          placeholder="Number"
-          value={formData.number}
-          onChange={handleChangeInput}
-        />
-        <button type="submit">Add contact</button>
-      </form>
-    );
-  
-}
 
 export default ContactForm;
