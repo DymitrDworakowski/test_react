a76af65b9291746ad6d6c6664f32a9f17c85dbf1
DymitrDworakowski <116100679+DymitrDworakowski@users.noreply.github.com>
2024-01-22 13:34:40 +0100

    components

---
+++ n6/src/components/Home/Home.jsx
@@ -0,0 +1,10 @@
+//компонент Home, домашня сторінка зі списком популярних кінофільмів.
+
+const Home = () => {
+    return (
+        <div>Hope page</div>
+    )
+};
+
+
+export default Home;
---
+++ n6/src/components/Movies/Movies.jsx
@@ -0,0 +1,9 @@
+//компонент Movies, сторінка пошуку кінофільмів за ключовим словом.
+
+const Movies = () => {
+    return (
+        <div>Movies </div>
+    )
+}
+
+export default Movies;
