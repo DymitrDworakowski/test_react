fb8fc6700758181da588b1a17a8e08eba0779d9a
DymitrDworakowski <116100679+DymitrDworakowski@users.noreply.github.com>
2024-01-23 16:27:11 +0100

    Update Movies.jsx

--- n6/src/components/Movies/Movies.jsx
+++ n6/src/components/Movies/Movies.jsx
@@ -7,6 +7,7 @@ const Movies = () => {
     const [movie, setMovie ]= useState([]);
     const [ query, setQuery ] = useState('');
 
+    // handleSubmit: Ця функція викликається при надсиланні форми пошуку. Вона отримує значення введеного тексту з форми, викликає функцію onSubmit та скидає форму.
   const handleSubmit = (evt) => {
     evt.preventDefault();
       const searchItem = formRef.current.elements.searchText.value;
@@ -14,7 +15,7 @@ const Movies = () => {
     onSubmit(searchItem);
     formRef.current.reset();
   };
-
+// getByName: Ця асинхронна функція викликає функцію getByNameMovies з параметром query (клічка пошуку). Вона отримує результат пошуку і, якщо він не порожній, оновлює стан movie знайденими фільмами.
     const getByName = useCallback(async () => {
     try {
       
@@ -29,12 +30,14 @@ const Movies = () => {
     } 
   }, [ setMovie,query ]);
 
+    // useEffect: Цей ефект викликається при зміні query або функції getByName. Якщо значення query не порожнє, він викликає функцію getByName.
 useEffect(() => {
     if (query.trim() !== "") {
       getByName();
     }
   }, [query, getByName]);
     
+    // onSubmit: Ця функція викликається при відправці форми пошуку. Вона перевіряє, чи не порожній введений текст та встановлює його в якості значення query.
     const onSubmit = (searchItem) => {
     if (searchItem.trim() === "") {
       alert("Input is empty");
