d6afa1d8bea5d2d1f12f9008c85099a250637704
DymitrDworakowski <116100679+DymitrDworakowski@users.noreply.github.com>
2024-01-27 15:58:26 +0100

    css add

--- n6/src/components/Header/Header.jsx
+++ n6/src/components/Header/Header.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import css from "./Header.module.css";
 
-const Header = (props) => {
+const Header = () => {
   return (
-    <nav>
-      <Link to="/" >
-        Home
-      </Link>
+    <nav className={css.nav}>
+      <Link to="/">Home</Link>
       <Link to="/movies">Movies</Link>
     </nav>
   );
--- n6/src/components/Home/Home.jsx
+++ n6/src/components/Home/Home.jsx
@@ -1,21 +1,24 @@
 import Loader from "../Loader/Loader";
 import { Link } from "react-router-dom";
+import css from "./Home.module.css";
 
 const Home = ({ movies, isLoading, error }) => {
   const http = "https://image.tmdb.org/t/p/w200";
   return (
-    <ul>
-      {isLoading && <Loader />}
-      {error && <p>Error: {error.message}</p>}
-      {movies.map(({ id, title, backdrop_path }) => (
-        <Link to={`/movies/${id}`} key={id}>
-          <li>
-            <h3>{title}</h3>
-            <img src={`${http}${backdrop_path}`} alt="Movie" />
-          </li>
-        </Link>
-      ))}
-    </ul>
+    <div className={css.film}>
+      <ul className={css.film_container}>
+        {isLoading && <Loader />}
+        {error && <p>Error: {error.message}</p>}
+        {movies.map(({ id, title, backdrop_path }) => (
+          <Link to={`/movies/${id}`} key={id}>
+            <li>
+              <h3>{title}</h3>
+              <img src={`${http}${backdrop_path}`} alt="Movie" />
+            </li>
+          </Link>
+        ))}
+      </ul>
+    </div>
   );
 };
 
