f1b4a77fa9bae6c5885fb802650fb693e50def6d
DymitrDworakowski <116100679+DymitrDworakowski@users.noreply.github.com>
2024-02-25 17:19:03 +0100

    style

--- n6/src/components/Header/Header.jsx
+++ n6/src/components/Header/Header.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import css from "./Header.module.css";
+import { ReactComponent as MovieIcon } from "../../img/movie.svg";
 
 const Header = () => {
   return (
     <div className={css.nav}>
-      <h1 className={css.logo}>LOGO</h1>
-      <NavLink className={css.home} to="/">
-        Home
-      </NavLink>
-      <NavLink className={css.movies} to="/movies">
-        Movies
-      </NavLink>
+      <MovieIcon className={css.logo} />
+      <div className={css.head_buttons}>
+        <NavLink className={css.home} to="/">
+          Home
+        </NavLink>
+        <NavLink className={css.movies} to="/movies">
+          Movies
+        </NavLink>
+      </div>
     </div>
   );
 };
