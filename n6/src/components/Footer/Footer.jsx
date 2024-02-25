e29056d0e57c203e2caffa18476a68fe22f460bb
DymitrDworakowski <116100679+DymitrDworakowski@users.noreply.github.com>
2024-02-25 16:49:24 +0100

    style add for movies

--- n6/src/components/Footer/Footer.jsx
+++ n6/src/components/Footer/Footer.jsx
@@ -1,9 +1,23 @@
+import React from "react";
+import css from "./Footer.module.css";
+import { ReactComponent as LinkedInIcon } from "../../img/linkedin.svg";
+
 const Footer = () => {
-    return (
-        <div>
-            <h1>Its Footer</h1>
-        </div>
-    )
+  return (
+    <div className={css.footer_container}>
+      <h1 className={css.footer_title}>Footer</h1>
+      <p className={css.developer_info}>
+        Developed by <LinkedInIcon className={css.linkedin_icon} />
+        <a
+          href="https://www.linkedin.com/in/dymitr-dworakowski/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Dymitr Dworakowski
+        </a>
+      </p>
+    </div>
+  );
 };
 
 export default Footer;
